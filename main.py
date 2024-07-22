e3c80c7205b58605773167e86c661b6c02a38646
Fabian Alegre <fabito.619@gmail.com>
2024-07-22 19:17:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,63 @@
 import google.generativeai as genai
-from flask import Flask, request, jsonify
+from flask import Flask,request,jsonify
 import requests
 import os
+import fitz
 
-wa_token = os.environ.get("WA_TOKEN")
+wa_token=os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
-model_name = "gemini-1.5-pro"
+model_name="gemini-1.5-pro"
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-model = genai.GenerativeModel(model_name=model_name)
-convo = model.start_chat(history=[])
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
 
-with open("instructions.txt", "r") as f:
-    commands = f.read()
+safety_settings = [
+  {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+  {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_MEDIUM_AND_ABOVE"},  
+  {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+  {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+]
+
+model = genai.GenerativeModel(model_name=model_name,
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[
+])
+
+with open("instructions.txt","r") as f:
+    commands=f.read()
 convo.send_message(commands)
 
-def send(answer, sender, phone_id):
-    url = f"https://graph.facebook.com/v18.0/{phone_id}/messages"
-    headers = {
+def send(answer,sender,phone_id):
+    url=f"https://graph.facebook.com/v18.0/{phone_id}/messages"
+    headers={
         'Authorization': f'Bearer {wa_token}',
         'Content-Type': 'application/json'
     }
-    data = {
-        "messaging_product": "whatsapp", 
-        "to": f"{sender}", 
-        "type": "text",
-        "text": {"body": f"{answer}"},
-    }
-    
-    response = requests.post(url, headers=headers, json=data)
+    data={
+          "messaging_product": "whatsapp", 
+          "to": f"{sender}", 
+          "type": "text",
+          "text":{"body": f"{answer}"},
+          }
+
+    response=requests.post(url, headers=headers,json=data)
     return response
 
-@app.route("/", methods=["GET", "POST"])
+def remove(*file_paths):
+    for file in file_paths:
+        if os.path.exists(file):
+            os.remove(file)
+        else:pass
+
+@app.route("/",methods=["GET","POST"])
 def index():
     return "Bot"
 
@@ -49,17 +74,47 @@ def webhook():
     elif request.method == "POST":
         try:
             data = request.get_json()["entry"][0]["changes"][0]["value"]["messages"][0]
-            phone_id = request.get_json()["entry"][0]["changes"][0]["value"]["metadata"]["phone_number_id"]
-            sender = "+" + data["from"]
+            phone_id=request.get_json()["entry"][0]["changes"][0]["value"]["metadata"]["phone_number_id"]
+            sender="+"+data["from"]
             if data["type"] == "text":
                 prompt = data["text"]["body"]
                 convo.send_message(prompt)
-                send(convo.last.text, sender, phone_id)
+                send(convo.last.text,sender,phone_id)
             else:
-                send("Lo siento, solo puedo procesar mensajes de texto por ahora.", sender, phone_id)
-        except Exception as e:
-            print(f"Error: {str(e)}")
+                media_url_endpoint = f'https://graph.facebook.com/v18.0/{data[data["type"]]["id"]}/'
+                headers = {'Authorization': f'Bearer {wa_token}'}
+                media_response = requests.get(media_url_endpoint, headers=headers)
+                media_url = media_response.json()["url"]
+                media_download_response = requests.get(media_url, headers=headers)
+                if data["type"] == "audio":
+                    filename = "/tmp/temp_audio.mp3"
+                elif data["type"] == "image":
+                    filename = "/tmp/temp_image.jpg"
+                elif data["type"] == "document":
+                    doc=fitz.open(stream=media_download_response.content,filetype="pdf")
+                    for _,page in enumerate(doc):
+                        destination="/tmp/temp_image.jpg"
+                        pix = page.get_pixmap()
+                        pix.save(destination)
+                        file = genai.upload_file(path=destination,display_name="tempfile")
+                        response = model.generate_content(["What is this",file])
+                        answer=response._result.candidates[0].content.parts[0].text
+                        convo.send_message(f"Direct image input has limitations, so this message is created by an llm model based on the image prompt of user, reply to the user assuming you saw that image: {answer}")
+                        send(convo.last.text,sender,phone_id)
+                        remove(destination)
+                else:send("This format is not Supported by the bot ☹",sender,phone_id)
+                with open(filename, "wb") as temp_media:
+                    temp_media.write(media_download_response.content)
+                file = genai.upload_file(path=filename,display_name="tempfile")
+                response = model.generate_content(["What is this",file])
+                answer=response._result.candidates[0].content.parts[0].text
+                remove("/tmp/temp_image.jpg","/tmp/temp_audio.mp3")
+                convo.send_message(f"Direct media input has limitations, so this is a voice/image message from user which is transcribed by an llm model, reply to the user assuming you heard/saw media file: {answer}")
+                send(convo.last.text,sender,phone_id)
+                files=genai.list_files()
+                for file in files:
+                    file.delete()
+        except :pass
         return jsonify({"status": "ok"}), 200
-
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
